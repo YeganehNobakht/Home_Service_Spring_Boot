c0abea48ddb13e261dd9f142aec715025896ca8e
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-26 20:04:25 +0430

    implementing sendVerificationEmail and verify methods to UserServiceImpl.

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/userService/UserServiceImpl.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/userService/UserServiceImpl.java
@@ -2,17 +2,20 @@ package ir.maktab.homeservices.service.userService;
 
 import ir.maktab.homeservices.data.entity.ServiceCategory;
 import ir.maktab.homeservices.data.entity.User;
+import ir.maktab.homeservices.data.entity.enums.UserStatus;
 import ir.maktab.homeservices.data.repository.User.UserRepository;
-import ir.maktab.homeservices.dto.ServiceCategoryDto;
 import ir.maktab.homeservices.dto.UserDto;
 import ir.maktab.homeservices.dto.UserFilter;
+import ir.maktab.homeservices.dto.enums.UserRole;
 import ir.maktab.homeservices.exceptions.checkes.ServiceNotFoundException;
-import ir.maktab.homeservices.service.mapper.Mapper;
 import ir.maktab.homeservices.service.mapper.UserMapper;
-import ir.maktab.homeservices.service.serviceCategory.ServiceCategoryService;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,37 +24,67 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final Mapper mapper;
-    private final ServiceCategoryService serviceCategoryService;
+    private final JavaMailSender mailSender;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, Mapper mapper, ServiceCategoryService serviceCategoryService) {
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, JavaMailSender mailSender) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
-        this.mapper = mapper;
-        this.serviceCategoryService = serviceCategoryService;
+        this.mailSender = mailSender;
     }
 
+    @Override
+    public void sendVerificationEmail(UserDto dto, String siteURL)
+            throws UnsupportedEncodingException, MessagingException {
+        String toAddress = dto.getEmail();
+        String fromAddress = "masimasi128@gmail.com";
+        String senderName = "masi";
+        String subject = "Please verify your registration";
+        String content = "Dear [[name]],<br>"
+                + "Please click the link below to verify your registration:<br>"
+                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
+                + "Thank you,<br>"
+                + "zahra asgari...";
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(fromAddress, senderName);
+        helper.setTo(toAddress);
+        helper.setSubject(subject);
+        content = content.replace("[[name]]", dto.getName());
+        String verifyURL = siteURL + "/verify/" + dto.getVerificationCode();
+        content = content.replace("[[URL]]", verifyURL);
+        helper.setText(content, true);
+        System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+        mailSender.send(message);
+    }
 
     @Override
-    public List<UserDto> filterUser(UserFilter user) throws ServiceNotFoundException {
+    public boolean verify(String verificationCode) {
+        User user = userRepository.findByVerificationCode(verificationCode);
+
+        if (user == null || user.isEnabled()) {
+            return false;
+        } else {
+            user.setVerificationCode(null);
+            user.setEnabled(true);
+            if (user.getUserRole().equals(UserRole.Customer))
+                user.setUserStatus(UserStatus.APPROVE);
+            if (user.getUserRole().equals(UserRole.Specialist))
+                user.setUserStatus(UserStatus.WAITING);
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    @Override
+    public List<UserDto> filterUser(UserFilter user, ServiceCategory service) throws ServiceNotFoundException {
         List<UserDto> userDtoList = new ArrayList<>();
-        ServiceCategoryDto serviceCategoryDto =null;
-        if (!StringUtils.isEmpty(user.getSpeciality())) {
-            serviceCategoryDto = serviceCategoryService.getByName(user.getSpeciality());
-        }
-        ServiceCategory service = null;
-        if (serviceCategoryDto!=null){
-            service = mapper.toServiceCategory(serviceCategoryDto);
-        }
         userDtoList.addAll(
-                userRepository.findAll(UserRepository.filterUsers(user,service))
+                userRepository.findAll(UserRepository.filterUsers(user, service))
                         .stream().map(userMapper::toUserDto).collect(Collectors.toList())
         );
-
-//        userDtoList.addAll(
-//                userRepository.findAll(UserRepository.filterSpecialists(user))
-//                        .stream().map(userMapper::toUserDto).collect(Collectors.toList())
-//        );
 
         return userDtoList;
     }
