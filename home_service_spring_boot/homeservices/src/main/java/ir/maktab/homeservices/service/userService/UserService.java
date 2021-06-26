044ef57b5828c12f82c2597d536cd62cfd209ff9
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-26 20:03:28 +0430

    adding sendVerificationEmail and verify methods to UserRepository.

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/userService/UserService.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/userService/UserService.java
@@ -1,14 +1,22 @@
 package ir.maktab.homeservices.service.userService;
 
+import ir.maktab.homeservices.data.entity.ServiceCategory;
 import ir.maktab.homeservices.dto.UserDto;
 import ir.maktab.homeservices.dto.UserFilter;
 import ir.maktab.homeservices.exceptions.checkes.ServiceNotFoundException;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public interface UserService {
 
-    List<UserDto> filterUser(UserFilter user) throws ServiceNotFoundException;
+    void sendVerificationEmail(UserDto dto, String siteURL)
+            throws UnsupportedEncodingException, MessagingException;
+
+    boolean verify(String verificationCode);
+
+    List<UserDto> filterUser(UserFilter user, ServiceCategory serviceCategory) throws ServiceNotFoundException;
 
     List<UserDto> findAll();
 }
