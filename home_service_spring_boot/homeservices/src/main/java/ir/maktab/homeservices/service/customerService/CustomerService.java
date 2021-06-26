0870dfdb8b82b8baadce3fea0f9debf2209bbac9
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-26 20:11:53 +0430

    adding registerCustomer method to CustomerService interface.

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/customerService/CustomerService.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/customerService/CustomerService.java
@@ -5,6 +5,8 @@ import ir.maktab.homeservices.dto.CustomerDto;
 import ir.maktab.homeservices.dto.CustomerOrderDto;
 import ir.maktab.homeservices.exceptions.checkes.*;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public interface CustomerService {
@@ -13,6 +15,8 @@ public interface CustomerService {
 
     Customer changePassword(String username, String oldPass, String newPass) throws CustomerNotFoundException, PasswordNotFoundException;
 
+    CustomerDto registerCustomer(CustomerDto customerDto, String siteURL) throws UnsupportedEncodingException, MessagingException, DuplicateEmailException, DuplicateUsernameException;
+
     CustomerDto create(CustomerDto customerOrderDto) throws DuplicateEmailException, DuplicateUsernameException;
 
     CustomerDto login(CustomerDto customerDto) throws PasswordNotFoundException, UserNameNotFoundException;
@@ -20,8 +24,7 @@ public interface CustomerService {
     List<CustomerOrderDto> getOrders(CustomerDto customerDto) throws Exception;
 
 
-
     void payByAccount(CustomerDto customerDto, double price);
 
-    void updateBalance(Double balance,Integer id);
+    void updateBalance(Double balance, Integer id);
 }
