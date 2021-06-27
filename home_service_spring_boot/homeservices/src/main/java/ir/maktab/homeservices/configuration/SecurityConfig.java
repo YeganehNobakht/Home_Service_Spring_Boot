0cc0c949ff166d1b8da7a0a81467f04ed8a6b863
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-27 08:53:59 +0430

    adding SecurityConfig.

---
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/configuration/SecurityConfig.java
@@ -0,0 +1,59 @@
+package ir.maktab.homeservices.configuration;
+
+import ir.maktab.homeservices.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","/customer/register","/verify/**", "/login", "/customer/signUp","/specialist/registerSignUp","/specialist/signUp", "/resources/**","/captcha").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").usernameParameter("username").passwordParameter("password")
+                .loginProcessingUrl("/userLogin")
+                .successHandler(authenticationSuccessHandler)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+}
