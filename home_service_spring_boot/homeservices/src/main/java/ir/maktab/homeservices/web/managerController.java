127f8ca8490a6c5f6b1f51eeb96f5518f7a20b8d
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-27 09:36:11 +0430

    fixing method responsible to login in managerController.

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/web/managerController.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/web/managerController.java
@@ -15,13 +15,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -48,74 +46,74 @@ public class managerController {
         return new ModelAndView("managerLogin", "managerDto", new ManagerDto());
     }
 
-    @PostMapping("/register")
-    public String managerLogin
-            (@ModelAttribute("managerDto") @Valid ManagerDto managerDto,
-             BindingResult bindingResult, Model model,
-             HttpServletRequest request) throws ManagerNotFoundException {
+    @GetMapping("/register")
+    public String managerLogin(HttpServletRequest request) throws ManagerNotFoundException, SpecialistNotFoundException {
         logger.info("...manager register...");
-        ManagerDto myManagerDto = managerService.login(managerDto);
-        HttpSession session = request.getSession(true);
-        session.setAttribute("myManagerDto", myManagerDto);
+        HttpSession session = request.getSession(false);
+        ManagerDto dto = managerService.findByUsername((String) session.getAttribute("username"));
+
+        session.setAttribute("myManagerDto", dto);
         return "managerService";
     }
 
     @GetMapping("/userInfo")
-    public ModelAndView getUsers(Model model, HttpServletRequest request){
+    public ModelAndView getUsers(Model model, HttpServletRequest request) {
         logger.info("...show user informations...");
         List<UserDto> allUser = userService.findAll();
-        model.addAttribute("allUser",allUser);
+        model.addAttribute("allUser", allUser);
         List<ServiceCategoryDto> all = serviceCategoryService.getAll();
         HttpSession session = request.getSession();
-        session.setAttribute("services",all);
-        session.setAttribute("role",new UserRole[]{UserRole.Customer, UserRole.Specialist});
-        return new ModelAndView("managerGetUserInfo","userDto",new UserFilter());
+        session.setAttribute("services", all);
+        session.setAttribute("role", new UserRole[]{UserRole.Customer, UserRole.Specialist});
+        return new ModelAndView("managerGetUserInfo", "userDto", new UserFilter());
     }
 
     @PostMapping("/search")
     public ModelAndView searchSuggestions(@ModelAttribute("userDto") UserFilter userFilter) throws ServiceNotFoundException {
         logger.info("...filter for user information...");
-        return new ModelAndView("managerGetUserInfo","allUser",managerService.filterUser(userFilter));
+        return new ModelAndView("managerGetUserInfo", "allUser", managerService.filterUser(userFilter));
     }
 
     @GetMapping("/addService")
-    public ModelAndView addService(){
+    public ModelAndView addService() {
         logger.info("...adding service...");
-        return new ModelAndView("managerAddService","serviceDto",new ServiceCategoryDto());
+        return new ModelAndView("managerAddService", "serviceDto", new ServiceCategoryDto());
     }
 
     @GetMapping("/subService")
-    public ModelAndView addSubService(Model model){
+    public ModelAndView addSubService(Model model) {
         logger.info("...adding sub-service...");
         List<ServiceCategoryDto> all = serviceCategoryService.getAll();
-        model.addAttribute("serviceList",all);
-        return new ModelAndView("managerAddSubService","subServiceDto",new SubCategoryDto());
+        model.addAttribute("serviceList", all);
+        return new ModelAndView("managerAddSubService", "subServiceDto", new SubCategoryDto());
     }
 
     @GetMapping("/Speciality")
-    public String showAllSpecialistAndTheirSpeciality(Model model){
+    public String showAllSpecialistAndTheirSpeciality(Model model) {
         List<SpecialistDto> specialistDtoList = specialistService.findAll();
 
-        model.addAttribute("specialistDtoList",specialistDtoList);
+        model.addAttribute("specialistDtoList", specialistDtoList);
         return "managerAddSpeciality";
     }
+
     @GetMapping("/addSpeciality/{id}")
     public String addSpecialityToSpecialist(@PathVariable("id") Integer id, HttpServletRequest request,
                                             Model model) throws SpecialistNotFoundException, ServiceAlreadyExistException {
         SpecialistDto specialistDto = specialistService.findById(id);
         HttpSession session = request.getSession(true);
-        session.setAttribute("specialistForAddSpeciality",specialistDto);
+        session.setAttribute("specialistForAddSpeciality", specialistDto);
         List<ServiceCategoryDto> serviceList = serviceCategoryService.getAll();
-        model.addAttribute("serviceList",serviceList);
-        session.setAttribute("serviceList",serviceList);
+        model.addAttribute("serviceList", serviceList);
+        session.setAttribute("serviceList", serviceList);
         return "managerAddingSpeciality";
     }
+
     @PostMapping("/addingSpecialist")
-    public String addingSpeciality(@SessionAttribute("specialistForAddSpeciality")SpecialistDto specialistDto,
-                                   @RequestParam("service")String serviceName,
+    public String addingSpeciality(@SessionAttribute("specialistForAddSpeciality") SpecialistDto specialistDto,
+                                   @RequestParam("service") String serviceName,
                                    Model model) throws SpecialistNotFoundException, ServiceAlreadyExistException {
-        serviceCategoryService.addServiceToSpecialist(serviceName,specialistDto);
-        model.addAttribute("success",maktabMessageSource.getEnglish("service.to.specialist.add",new Object[]{serviceName}));
+        serviceCategoryService.addServiceToSpecialist(serviceName, specialistDto);
+        model.addAttribute("success", maktabMessageSource.getEnglish("service.to.specialist.add", new Object[]{serviceName}));
         return "managerSuccessPage";
 
     }
