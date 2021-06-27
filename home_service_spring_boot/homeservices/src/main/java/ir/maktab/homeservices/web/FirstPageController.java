6597670b17860a1c4be8921e12b9cd9fac4d8448
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-27 08:57:52 +0430

    adding login controller to FirstPageController class.

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/web/FirstPageController.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/web/FirstPageController.java
@@ -1,10 +1,17 @@
 package ir.maktab.homeservices.web;
 
+import ir.maktab.homeservices.dto.SpecialistDto;
+import ir.maktab.homeservices.dto.UserDto;
+import ir.maktab.homeservices.service.validation.OnLogin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Yeganeh Nobakht
@@ -17,6 +24,17 @@ public class FirstPageController {
     public String home() {
         logger.info("...show first page when application is started...");
         return "index";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView showLoginPage(){
+        logger.info("...login page...");
+        return new ModelAndView("login","userDto", new UserDto());
+    }
+    @PostMapping("/userLogin")
+    public ModelAndView Login(@ModelAttribute("userDto") @Validated(OnLogin.class) UserDto userDto){
+        logger.info("...login page...");
+        return new ModelAndView("customerService","userDto", new UserDto());
     }
 
 }
