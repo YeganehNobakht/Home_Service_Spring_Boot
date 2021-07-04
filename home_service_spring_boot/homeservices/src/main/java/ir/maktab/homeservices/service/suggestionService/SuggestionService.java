7d90b70d8c827c27922ceb9b33d763ad2097c83a
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-07-04 13:27:24 +0430

    adding showOrderWaitForOffer method to SuggestionService interface

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/suggestionService/SuggestionService.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/suggestionService/SuggestionService.java
@@ -3,15 +3,17 @@ package ir.maktab.homeservices.service.suggestionService;
 
 import ir.maktab.homeservices.data.entity.enums.SuggestionStatus;
 import ir.maktab.homeservices.dto.CustomerOrderDto;
+import ir.maktab.homeservices.dto.OrderDto;
 import ir.maktab.homeservices.dto.SpecialistDto;
 import ir.maktab.homeservices.dto.SuggestionDto;
 import ir.maktab.homeservices.exceptions.checkes.*;
 
 import java.util.List;
+import java.util.Set;
 
 public interface SuggestionService {
 
-    void addSuggestionForOrder(SuggestionDto suggestionDto, CustomerOrderDto customerOrderDto) throws TooLowSubServicePriceException;
+    void addSuggestionForOrder(SuggestionDto suggestionDto, CustomerOrderDto customerOrderDto) throws TooLowSubServicePriceException, OrderNotFoundException;
 
     public List<SuggestionDto> filterSuggestion(SuggestionDto suggestionDto);
 
@@ -30,4 +32,6 @@ public interface SuggestionService {
     List<SuggestionDto> findUserBySuggestionStatusAndSpecialist(SuggestionStatus status, Integer id);
 
     void selectASuggestion(Integer id,CustomerOrderDto orderDto) throws OrderException, OrderNoSuggestionException, OrderNotFoundException, SuggestionNotFoundException;
+
+    Set<CustomerOrderDto> showOrderWaitForOffer(SpecialistDto specialistDto);
 }
