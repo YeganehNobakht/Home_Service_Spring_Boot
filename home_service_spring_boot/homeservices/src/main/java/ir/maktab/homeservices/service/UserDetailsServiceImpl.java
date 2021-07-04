104b6bbfaf97b98e61fe8683318cbd844c52fb05
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-07-04 13:25:49 +0430

    adding message source to UserDetailsServiceImpl

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/UserDetailsServiceImpl.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package ir.maktab.homeservices.service;
 
 import ir.maktab.homeservices.data.repository.User.UserRepository;
 import ir.maktab.homeservices.dto.enums.UserRole;
+import ir.maktab.homeservices.service.maktabMassageSource.MaktabMessageSource;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,10 +15,13 @@ import java.util.Optional;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final MaktabMessageSource maktabMessageSource;
 
-    public UserDetailsServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository, MaktabMessageSource maktabMessageSource) {
         this.userRepository = userRepository;
+        this.maktabMessageSource = maktabMessageSource;
     }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -31,16 +35,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                             .roles(UserRole.Customer.name())
                             .build();
                 }
-//                TODO::Exception
             }
             if (user.getUserRole().equals(UserRole.Specialist)) {
+
                 if (user.isEnabled()) {
                     return User.withUsername(user.getUsername())
                             .password(user.getPassword())
                             .roles(UserRole.Specialist.name())
                             .build();
                 }
-//                TODO::Exception
+//
             }
             if (user.getUserRole().equals(UserRole.Manager)) {
                 return User.withUsername(user.getUsername())
@@ -50,8 +54,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             }
 
         }
-        //TODO:: message source
-        throw new UsernameNotFoundException("Customer not found.");
+        throw new UsernameNotFoundException(maktabMessageSource.getEnglish("user.not.found1"));
 
     }
 }
