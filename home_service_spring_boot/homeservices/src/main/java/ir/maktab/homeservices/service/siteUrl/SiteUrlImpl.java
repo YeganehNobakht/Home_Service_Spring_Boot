15f89a611e6271e4d19dd9d66fe2556749c69c99
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-07-04 13:18:25 +0430

    implementing Site url for sending verify email to users

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/siteUrl/SiteUrlImpl.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/service/siteUrl/SiteUrlImpl.java
@@ -3,8 +3,9 @@ package ir.maktab.homeservices.service.siteUrl;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+
 @Service
-public class SiteUrlImpl implements SiteUrl{
+public class SiteUrlImpl implements SiteUrl {
     @Override
     public String getSiteURL(HttpServletRequest request) {
         String siteURL = request.getRequestURL().toString();
