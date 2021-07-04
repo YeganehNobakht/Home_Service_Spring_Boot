0c4f9df39b20dd7c0082e85535dd83bea5e5115f
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-07-04 13:56:44 +0430

    adding /accessdenied url in controller

--- home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/web/FirstPageController.java
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/web/FirstPageController.java
@@ -1,11 +1,11 @@
 package ir.maktab.homeservices.web;
 
-import ir.maktab.homeservices.dto.SpecialistDto;
 import ir.maktab.homeservices.dto.UserDto;
 import ir.maktab.homeservices.service.validation.OnLogin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class FirstPageController {
     private final Logger logger = LogManager.getLogger(FirstPageController.class);
+
     @GetMapping
     public String home() {
         logger.info("...show first page when application is started...");
@@ -27,14 +28,20 @@ public class FirstPageController {
     }
 
     @GetMapping("/login")
-    public ModelAndView showLoginPage(){
+    public ModelAndView showLoginPage() {
         logger.info("...login page...");
-        return new ModelAndView("login","userDto", new UserDto());
-    }
-    @PostMapping("/userLogin")
-    public ModelAndView Login(@ModelAttribute("userDto") @Validated(OnLogin.class) UserDto userDto){
-        logger.info("...login page...");
-        return new ModelAndView("customerService","userDto", new UserDto());
+        return new ModelAndView("login", "userDto", new UserDto());
     }
 
+    @PostMapping("/userLogin")
+    public ModelAndView Login(@ModelAttribute("userDto") @Validated(OnLogin.class) UserDto userDto) {
+        logger.info("...login page...");
+        return new ModelAndView("customerService", "userDto", new UserDto());
+    }
+
+    @GetMapping("/accessdenied")
+    public String loginerror(ModelMap model) {
+        model.addAttribute("error", "true");
+        return "denied";
+    }
 }
