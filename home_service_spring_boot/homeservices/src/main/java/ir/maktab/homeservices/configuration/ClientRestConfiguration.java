d12fd51afd9e74af24fe14c6eb888ebdc0fe5120
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-07-04 12:48:15 +0430

    adding ClientRestConfiguration for using map SDK for determining customer location

---
+++ home_service_spring_boot/homeservices/src/main/java/ir/maktab/homeservices/configuration/ClientRestConfiguration.java
@@ -0,0 +1,21 @@
+package ir.maktab.homeservices.configuration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Controller
+public class ClientRestConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+        return builder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+    }
+}
